04271675022d164d7371c85053351d7f367d3655
DCKAP <varshithaksdckap@gmail.com>
2023-10-13 16:06:27 +0530

    game

---
+++ index.js
@@ -0,0 +1,73 @@
+// let person1 = prompt("Enter Rock or Paper or Secissor for person1");
+// let person2 = prompt ("Enter Rock or Paper or Secissor for person2");
+// let a = "rock";
+// let b = "paper";
+// let c = "secissor";
+// if (person1 == a && person2 == c || person1 == b && person2 == a || person1 == c && person2 == b)
+// {
+//     alert("person1 is winner")
+// }
+// else if (person1 == a && person2 == b || person1 == b && person2 == c || person1 == c && person2 == a)
+// {
+//     alert("person2 is winner")
+// }
+// else
+// {
+//     alert("Both are winner");
+// }
+
+//------------------------------------Another method------------------------------------------
+// let person1 = prompt("Enter Rock or Paper or Secissor for person1");
+// let person2 = prompt ("Enter Rock or Paper or Secissor for person2");
+// let a = "rock";
+// let b = "paper";
+// let c = "secissor";
+// if (person1 == a)
+// {
+//     if (person2 == c)
+//     {
+//         alert("person1 is winner")
+//     }
+//     else if (person2 == b)
+//     {
+//         alert("person2 is winner");
+//     }
+//     else if (person2 == a)
+//     {
+//         alert("both are winner")
+//     }
+// }
+// else if (person1 == b)
+// {
+//     if (person2 == a)
+//     {
+//         alert("person1 is winner")
+//     }
+//     else if (person2 == c)
+//     {
+//         alert("person2 is winner")
+//     }
+//     else if (person2 == b)
+//     {
+//         alert("both are winer")
+//     }
+// }
+// else if (person1 == c)
+// {
+//     if (person2 == b)
+//     {
+//         alert("person1 is winner")
+//     }
+//     else if (person2 == a)
+//     {
+//         alert("person2 is winner")
+//     }
+//     else if (person2 == c)
+//     {
+//         alert("both are winner")
+//     }
+// }
+// else
+// {
+//     alert("Enter a value");
+// }
